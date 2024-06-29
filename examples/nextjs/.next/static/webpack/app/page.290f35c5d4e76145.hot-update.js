fb0e12e52445da742e29dc70f127c4e4d80a7a66
wengcan <572037922@qq.com>
2024-06-29 23:57:16 +0800

    use turbopack

---
+++ examples/nextjs/.next/static/webpack/app/page.290f35c5d4e76145.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FVolumes%2FSANDISK%2FWork%2Fadvance-text-effects%2Fexamples%2Fnextjs%2Fapp%2Fhooks%2FuseAdvancedTypingAnimation.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!":
+/*!******************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FVolumes%2FSANDISK%2FWork%2Fadvance-text-effects%2Fexamples%2Fnextjs%2Fapp%2Fhooks%2FuseAdvancedTypingAnimation.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false! ***!
+  \******************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/hooks/useAdvancedTypingAnimation.js */ \"(app-pages-browser)/./app/hooks/useAdvancedTypingAnimation.js\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uLi8uLi9ub2RlX21vZHVsZXMvbmV4dC9kaXN0L2J1aWxkL3dlYnBhY2svbG9hZGVycy9uZXh0LWZsaWdodC1jbGllbnQtZW50cnktbG9hZGVyLmpzP21vZHVsZXM9JTdCJTIycmVxdWVzdCUyMiUzQSUyMiUyRlZvbHVtZXMlMkZTQU5ESVNLJTJGV29yayUyRmFkdmFuY2UtdGV4dC1lZmZlY3RzJTJGZXhhbXBsZXMlMkZuZXh0anMlMkZhcHAlMkZob29rcyUyRnVzZUFkdmFuY2VkVHlwaW5nQW5pbWF0aW9uLmpzJTIyJTJDJTIyaWRzJTIyJTNBJTVCJTIyZGVmYXVsdCUyMiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLDRNQUFvSyIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzY1ZGUiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIsIHdlYnBhY2tFeHBvcnRzOiBbXCJkZWZhdWx0XCJdICovIFwiL1ZvbHVtZXMvU0FORElTSy9Xb3JrL2FkdmFuY2UtdGV4dC1lZmZlY3RzL2V4YW1wbGVzL25leHRqcy9hcHAvaG9va3MvdXNlQWR2YW5jZWRUeXBpbmdBbmltYXRpb24uanNcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/../../node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FVolumes%2FSANDISK%2FWork%2Fadvance-text-effects%2Fexamples%2Fnextjs%2Fapp%2Fhooks%2FuseAdvancedTypingAnimation.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
